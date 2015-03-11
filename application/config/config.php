303a56ffc9ef9869498e0fe2e0dded203c3533d4
shaina ashraf <shaina2231.ashraf@gmail.com>
2015-03-12 01:11:50 +0500

    config changes

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://smartclass.herokuapp.com/';
+$config['base_url']	= 'https://smartclass.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
